2c349672d3110e4d8f40d252fe95907877d3ebd4
BCarnaval <antoinedelagrave@hotmail.com>
2022-08-28 22:17:26 -0400

    shell scripts init

---
+++ install.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+# Colors used in terminal messages
+RED="$(printf '\033[31m')"
+GREEN="$(printf '\033[32m')"
+ORANGE="$(printf '\033[33m')"
+CYAN="$(printf '\033[36m')"
+WHITE="$(printf '\033[37m')"
+
+# Paths
+PROJ=UniTeX
+CURRENT_DIR=$( pwd )
+DESTINATION=/usr/local/share
+
+build_directory () {
+    echo -e "${GREEN}[@] Installing ${PROJ}..."
+    if [[ -d "${DESTINATION}"/UniTeX ]]; then
+        # Updating directories
+        sudo rm -rf ${DESTINATION}/${PROJ}
+        sudo mkdir -p ${DESTINATION}/${PROJ}
+    else
+        sudo mkdir -p ${DESTINATION}/${PROJ}
+    fi
+}
+
+fill_directory () {
+    sudo cp -r ${CURRENT_DIR}/unitex.sh ${DESTINATION}/${PROJ}
+    sudo cp -r ${CURRENT_DIR}/intall.sh ${DESTINATION}/${PROJ}
+    sudo cp -r ${CURRENT_DIR}/uninstall.sh ${DESTINATION}/${PROJ}
+    sudo cp -r ${CURRENT_DIR}/man_page.1 ${DESTINATION}/${PROJ}
+
+    # Make scripts executable
+    sudo chmod +x ${DESTINATION}/${PROJ}/unitex.sh
+    sudo chmod +x ${DESTINATION}/${PROJ}/install.sh
+    sudo chmod +x ${DESTINATION}/${PROJ}/uninstall.sh
+
+    # Create symlink
+    if [[ -L /usr/local/bin/unitex ]]; then
+        sudo rm /usr/local/bin/unitex
+        sudo ln -s ${DESTINATION}/${PROJ}/unitex.sh /usr/local/bin/unitex
+    else
+        sudo ln -s ${DESTINATION}/${PROJ}/unitex.sh /usr/local/bin/unitex
+    fi
+    echo -e "${GREEN}[@] ${WHITE} ${PROJ} ${GREEN} Installed successfully:${WHITE} Execute 'unitex' to verify installation."
+}
+
+main () {
+    build_directory
+    fill_directory
+}
+
+main
---
+++ uninstall.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Colors used in terminal messages
+RED="$(printf '\033[31m')"
+GREEN="$(printf '\033[32m')"
+ORANGE="$(printf '\033[33m')"
+CYAN="$(printf '\033[36m')"
+WHITE="$(printf '\033[37m')"
+
+PROJ=UniTeX
+DESTINATION=/usr/local/share
+
+rm_directory () {
+    echo -e "${ORANGE}[!] Uninstalling ${PROJ}..."
+    if [[ -d ${DESTINATION}/${PROJ} ]]; then
+        sudo rm -rf ${DESTINATION}/${PROJ}
+    fi
+}
+
+rm_symlink() {
+    if [[ -L /usr/local/bin/unitex ]]; then
+        sudo rm /usr/local/bin/unitex
+    fi
+    echo -e "${GREEN}[@] ${WHITE} ${PROJ} ${GREEN} Uninstalled successfully."
+}
+
+main () {
+    rm_directory
+    rm_symlink
+}
+
+main
