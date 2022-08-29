d0bdbfc01c62c7026bc3abc72f77f44796bf4908
BCarnaval <antoinedelagrave@hotmail.com>
2022-08-28 22:43:36 -0400

    unitex init

--- unitex.sh
+++ unitex.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
 
+# Author: Antoine de Lagrave
+# Email: antoinedelagrave@hotmail.com
+# GitHub: @BCarnaval
+
+# UniTeX: UniTeX is a collection of scientific oriented and minimalistic 
+# LaTeX templates suitable for many assignement types.
+
+
