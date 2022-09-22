cf4bcacdef678627a5624b1b96b5c2024f07c9bc
BCarnaval <antoinedelagrave@hotmail.com>
2022-09-22 10:52:04 -0400

    Trailing whitespaces...

--- unitex.sh
+++ unitex.sh
@@ -4,7 +4,7 @@
 # Email: antoinedelagrave@hotmail.com
 # GitHub: @BCarnaval
 
-# UniTeX: UniTeX is a collection of scientific oriented and minimalistic 
+# UniTeX: UniTeX is a collection of scientific oriented and minimalistic
 # LaTeX templates suitable for many assignment types.
 
 RED="$(printf '\033[31m')"
@@ -28,15 +28,14 @@ usage () {
 
     cat << EOF
     ------------------------------------------------------------------------------
-    ------------------------------------------------------------------------------
     ${CYAN}
-    888     888          d8b 88888888888      Y88b   d88P 
-    888     888          Y8P     888           Y88b d88P  
-    888     888                  888            Y88o88P   
-    888     888 88888b.  888     888   .d88b.    Y888P    
-    888     888 888 "88b 888     888  d8P  Y8b   d888b    
-    888     888 888  888 888     888  88888888  d88888b   
-    Y88b. .d88P 888  888 888     888  Y8b.     d88P Y88b  
+    888     888          d8b 88888888888      Y88b   d88P
+    888     888          Y8P     888           Y88b d88P
+    888     888                  888            Y88o88P
+    888     888 88888b.  888     888   .d88b.    Y888P
+    888     888 888 "88b 888     888  d8P  Y8b   d888b
+    888     888 888  888 888     888  88888888  d88888b
+    Y88b. .d88P 888  888 888     888  Y8b.     d88P Y88b
      "Y88888P"  888  888 888     888   "Y8888 d88P   Y88b
     ${WHITE}
     Usage: ${CYAN}$(basename $0) ${WHITE}-h <help> -v <version> -b <build> -d <dir> -o <opt> [-hvbdo]
@@ -134,13 +133,13 @@ while getopts ":b:d:o:vh" opt; do
                             BUILD_DIR=$(pwd)
                             break
                             ;;
-                        No) 
+                        No)
                             reset_terminal
                             exit 0
                             ;;
                     esac
                 done
-                
+
             fi
             ;;
 
